3d9ebc62067a898f34d26f0c743e659ac15091d8
Ezy93 <giordani001@gmail.com>
2022-03-07 18:15:04 +0100

    ricerca di soluzioni per fermare il gioco al click di una bomba

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let squareElement = "";
 button.addEventListener("click" , function(){
     
     
-    resetButton.classList.remove("d-none")
+    /* resetButton.classList.remove("d-none") */
     if(selectedDifficulty.value === "easy"){
 
         for(let i = 0; i < 100; i++){
@@ -67,9 +67,9 @@ button.addEventListener("click" , function(){
 
 
 
-resetButton.addEventListener("click", function(){
+/* resetButton.addEventListener("click", function(){
     window.location.reload()
-})
+}) */
 
 /**
  * funzione che calcola la radice quadrata
@@ -81,7 +81,7 @@ function squareRoot(number){
 }
 
 document.querySelector("#punteggio").innerHTML = " ";
-let points = 0;
+let points = 1;
 
 /**
  * funzione che controlla se nel primo parametro inserito non è incluso l'innerhtml del secondo paramentro
@@ -95,6 +95,9 @@ function bombNoBomb(blackListArray,DOMElement){
         
     }else{
         DOMElement.classList.add("bomb")
+        document.querySelector("#punteggio").innerHTML = `hai cliccato su una mina hai perso!!! <br>
+        il tuo punteggio finale è: ${points - 1}`
+        return true
     }
 }
 
@@ -104,13 +107,16 @@ L'utente indica un livello di difficoltà in base al quale viene generata una gr
 con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
-Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. I numeri nella lista delle bombe non possono essere duplicati. //!check
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. I numeri nella lista delle bombe non possono essere duplicati.
 In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati
     - abbiamo calpestato una bomba
     - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b. */
 
+
+
+//?SEZIONE DEDICATA ALLE FUNZIONI E ALLE VARIABILI INIZIALIZZATE PER IL LORO FUNZIONAMENTO
 
 /* Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. 
     I numeri nella lista delle bombe non possono essere duplicati. */ //!check
@@ -128,6 +134,11 @@ function bombsGenerator(howMuch,level){
     while(flag){
         let randomInt = Math.floor(Math.random() * (level - 1) + 1)
         bombs.push(randomInt)
+
+        if(!bombs.includes(randomInt)){
+            bombs.push(randomInt)
+        }
+
         if(bombs.length == howMuch){
             flag = false;
         }
